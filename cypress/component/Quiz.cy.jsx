ae9296e975aa4d808c7ac882c664ae7bb2044a88
Arjun Patel <arjunpatel9217@gmail.com>
2024-12-18 18:43:20 -0600

    added component testing with cypress

---
+++ cypress/component/Quiz.cy.jsx
@@ -0,0 +1,8 @@
+import Quiz from '../../client/src/components/Quiz'
+
+describe('<Quiz />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<Quiz />)
+  })
+})
